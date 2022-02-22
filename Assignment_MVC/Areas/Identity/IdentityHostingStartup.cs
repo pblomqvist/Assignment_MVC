6b07bff6ffa906ea3a89563cd6edd314a133c8bc
pblomqvist <93123616+pblomqvist@users.noreply.github.com>
2022-02-22 11:20:59 +0100

    Identity scaffolding added

---
+++ Assignment_MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Assignment_MVC.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Assignment_MVC.Areas.Identity.IdentityHostingStartup))]
+namespace Assignment_MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
